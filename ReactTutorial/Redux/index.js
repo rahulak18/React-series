c64626c134422a660c4e3ba16ac4e606be4e0a83
rahulak18 <rahulakannur19@gmail.com>
2024-08-07 20:11:08 +0530

    Multiple reducers in a single store

--- ReactTutorial/Redux/index.js
+++ ReactTutorial/Redux/index.js
@@ -1,7 +1,9 @@
 
 const redux = require("redux");
 const createStore = redux.createStore;
+
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICE_CREAM = 'ICE_CREAM';
 
 // Action is a plain js object having type as a mandatory property
 // Action creation 
@@ -11,9 +13,16 @@ const buyCake = () => {
         info: "Buying a cake"
     }
 }
+const buyIceCreams = () => {
+    return {
+        type: BUY_ICE_CREAM,
+        info: "Buying a ice creams"
+    }
+}
 
 const initialState = {
-    numOfCakes: 10
+    numOfCakes: 10,
+    numofIceCreams: 20
 }
 
 const reducer = (state = initialState, action) => {
@@ -24,7 +33,13 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 numOfCakes: state.numOfCakes - 1
-            }
+            };
+
+        case BUY_ICE_CREAM:
+            return {
+                ...state,
+                numofIceCreams: state.numofIceCreams - 1
+            };
 
         default:
             return state;
@@ -41,6 +56,8 @@ const unsubscribe = store.subscribe(() => {
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCreams());
+store.dispatch(buyIceCreams());
 
 // unsubscribing
 unsubscribe();
