7efe6bfd366aa9cbc3009775ffd57111b23101f3
rahulak18 <rahulakannur19@gmail.com>
2024-08-16 19:37:12 +0530

    Initial setup --igonre

---
+++ ReactTutorial/react-redux-demo/src/Components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CakeContainer = () => {
+    return (
+        <div>
+            <p>Cake container</p>
+            <button>Click Me</button>
+        </div>
+    )
+}
+
+export default CakeContainer;
