07ff66f049b5a718486808ff8f7f43cb7298295f
rahulak18 <rahulakannur19@gmail.com>
2024-08-27 21:23:52 +0530

    Yup Validation Schema

--- ReactTutorial/react-formik-demo/src/Components/YoutubeForm.js
+++ ReactTutorial/react-formik-demo/src/Components/YoutubeForm.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const YoutubeForm = () => {
+
+    const validateSchema = Yup.object({
+        name: Yup.string().required('It is a required field!'),
+        email: Yup.string().email('Invalid email format man').required('It is a required field!'),
+        channel: Yup.string().required('It is a required field!')
+    })
 
     const formik = useFormik({
         initialValues: {
@@ -12,38 +19,41 @@ const YoutubeForm = () => {
         onSubmit: values => {
             console.log("values : ", values);
         },
-        validate: values => {
+        validationSchema: validateSchema
+        // validationSchema  -> we can write this only instead of the above line if the Yup object defined named is validationSchema (which is a ES6 syntax/feature)
 
-            let errors = {};
-
-            if (!values.name) {
-                errors.name = 'Required'
-            }
-
-            if (!values.email) {
-                errors.email = 'Required'
-            }
-
-            if (!values.channel) {
-                errors.channel = 'Required'
-            }
-            return errors;
-        }
     });
 
-    console.log("Formiks errors : ", formik.errors); // using this errors in the jsx conditionally to display the error message
+    console.log("Formik touched/visited fields : ", formik.touched); // gives the visited field in object with the property value as true if it is visited and blurred (we can use this formik.error to show error message, if that field value is required and user didnt entered values and moves to other field)
 
     return (
         <div>
             <form onSubmit={formik.handleSubmit}>
                 <label htmlFor='name'>Name</label>
-                <input type='text' id='name' name='name' value={formik.values.name} onChange={formik.handleChange} />
+                <input
+                    type='text'
+                    id='name'
+                    name='name'
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur} />
 
                 <label htmlFor='email'>E-mail</label>
-                <input type='email' id='email' name='email' onChange={formik.handleChange} value={formik.values.email} />
+                <input
+                    type='email'
+                    id='email'
+                    name='email'
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                    onBlur={formik.handleBlur} />
 
                 <label htmlFor='channel'>Channel</label>
-                <input type='text' id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel} />
+                <input type='text'
+                    id='channel'
+                    name='channel'
+                    onChange={formik.handleChange}
+                    value={formik.values.channel}
+                    onBlur={formik.handleBlur} />
 
                 <button type='submit'>Submit</button>
             </form>
